24cdc7340523bf5f9301159e537701239d4b3e25
Thaddea4 <adeyasyie@gmail.com>
2026-06-01 18:22:08 +0700

    mengecek file dan menampilkan isi file

--- StreamaReaderandWriter.cpp
+++ StreamaReaderandWriter.cpp
@@ -35,5 +35,17 @@ int main()
     infile.open("contohfile.txt");
 
     cout << endl << ">=Membuka dan membaca file " << endl;
+     
+    if (infile.is_open())
+    {
+        while (getline(infile, baris)) {
+            cout << baris << endl;
+        }
+       
+        infile.close();
+    }
+    
+    else cout << "Unable to open file";
+    return 0;
 
 }

c1cceaeb02acf07a3da043b5ed6cdbcfec99c5d9
Thaddea4 <adeyasyie@gmail.com>
2026-06-01 20:18:50 +0700

    menambahkan program utama dengan menu dan fiturnya

--- tugasPraktikum.cpp
+++ tugasPraktikum.cpp
@@ -49,3 +49,132 @@ void tampilData()
 
     file.close();
 }
+
+int main()
+{
+    TokoElektronik toko;
+    int pilih;
+
+    do
+    {
+        tampilData();
+
+        cout << "\n===== MENU =====\n";
+        cout << "1. Tambah Barang\n";
+        cout << "2. Update Barang\n";
+        cout << "3. Hapus Barang\n";
+        cout << "4. Simulasi Etalase\n";
+        cout << "5. Keluar\n";
+        cout << "Pilihan : ";
+        cin >> pilih;
+        cin.ignore();
+
+        if (pilih == 1)
+        {
+            ofstream file("gudang.txt", ios::app);
+            string barang;
+
+            cout << "Nama barang : ";
+            getline(cin, barang);
+
+            file << barang << endl;
+            file.close();
+
+            cout << "Data berhasil ditambah.\n";
+        }
+
+        else if (pilih == 2)
+        {
+            ifstream file("gudang.txt");
+            vector<string> data;
+            string barang;
+
+            while (getline(file, barang))
+            {
+                data.push_back(barang);
+            }
+            file.close();
+
+            int no;
+            cout << "Nomor barang yang diubah : ";
+            cin >> no;
+            cin.ignore();
+
+            if (no >= 1 && no <= data.size())
+            {
+                cout << "Nama baru : ";
+                getline(cin, data[no - 1]);
+
+                ofstream tulis("gudang.txt");
+
+                for (int i = 0; i < data.size(); i++)
+                {
+                    tulis << data[i] << endl;
+                }
+
+                tulis.close();
+                cout << "Data berhasil diubah.\n";
+            }
+        }
+
+        else if (pilih == 3)
+        {
+            ifstream file("gudang.txt");
+            vector<string> data;
+            string barang;
+
+            while (getline(file, barang))
+            {
+                data.push_back(barang);
+            }
+            file.close();
+
+            int no;
+            cout << "Nomor barang yang dihapus : ";
+            cin >> no;
+
+            if (no >= 1 && no <= data.size())
+            {
+                data.erase(data.begin() + (no - 1));
+
+                ofstream tulis("gudang.txt");
+
+                for (int i = 0; i < data.size(); i++)
+                {
+                    tulis << data[i] << endl;
+                }
+
+                tulis.close();
+                cout << "Data berhasil dihapus.\n";
+            }
+        }
+
+        else if (pilih == 4)
+        {
+            cout << "\nSkenario 1 (Rak 1)\n";
+
+            try
+            {
+                cout << toko.ambilProduk(1) << endl;
+            }
+            catch (string pesan)
+            {
+                cout << pesan << endl;
+            }
+
+            cout << "\nSkenario 2 (Rak 5)\n";
+
+            try
+            {
+                cout << toko.ambilProduk(5) << endl;
+            }
+            catch (string pesan)
+            {
+                cout << pesan << endl;
+            }
+        }
+
+    } while (pilih != 5);
+
+    return 0;
+}
